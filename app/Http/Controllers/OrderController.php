676fe13e0c5ab97bb7e680db3bb0db6411dd9954
Andrey <folganoid@yahoo.com>
2017-10-31 16:52:52 +0200

    (fnc)  view orders & order groups endup

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,39 +15,83 @@ class OrderController extends Controller
     /**
      * Index
      */
-    function index() {
+    function index()
+    {
 
         $id = Auth::id();
         $orders = User::find($id)->order; // orders for current user
 
-        $menus = [];        // array curent menus
-        $menuDesc = [];     // array of menu description
-        $groups = [];
 
-        for($i = 0 ; $i < count($orders); $i++ ) {
+
+        $ordersArr = $this->buildOrders($id, $orders);
+
+        $group = Group::where('user_id', $id)->get();
+        $groupList = [];
+
+
+        for( $i = 0 ; $i < count($group) ; $i++) {
+            $groupList[] = $group[$i]->order_id;
+        }
+
+        $groupOrders = Order::find($groupList);
+        $groupOwners = [];
+
+        for( $i = 0 ; $i < count($groupOrders) ; $i++) {
+            $groupOwners[$groupOrders[$i]->user_id] = User::where('id', $groupOrders[$i]->user_id)->first();
+        }
+
+        $groupArr = $this->buildOrders($id, $groupOrders);
+
+
+        return view('order')->with([
+            'orders' => $orders,
+            'menus' => $ordersArr['menus'],
+            'menuDesc' => $ordersArr['menuDesc'],
+            'groups' => $ordersArr['groups'],
+            'groupOrders' => $groupOrders,
+            'groupMenus' => $groupArr['menus'],
+            'groupMenuDesc' => $groupArr['menuDesc'],
+            'groupGroups' => $groupArr['groups'],
+            'groupOwners' => $groupOwners,
+        ]);
+
+    }
+
+    /**
+     * Build order list
+     *
+     * @param $id
+     * @param $orders
+     * @return array
+     */
+    public function buildOrders($id, $orders): array
+    {
+
+        $menus = [];        // array current menus
+        $menuDesc = [];     // array menu description
+        $groups = [];       // array group users with
+
+        for ($i = 0; $i < count($orders); $i++) {
 
             $orderId = $orders[$i]->id;
-
             $temp_group = Group::where('order_id', $orderId)->get();
 
-            for ($s = 0; $s < count($temp_group) ; $s++) {
-
+            for ($s = 0; $s < count($temp_group); $s++) {
                 $groups[$orderId][] = User::where('id', $temp_group[$s]->user_id)->first();
-
             }
 
             $menus[$orderId] = Order::find($orderId)->orderMenu;
 
-            for( $z = 0 ; $z < count($menus[$orderId]) ; $z++) {
-
+            for ($z = 0; $z < count($menus[$orderId]); $z++) {
                 $menuId = $menus[$orderId][$z]->menu_id;
                 $menuDesc[$menuId] = Menu::where('id', $menuId)->first();
             }
         }
 
-       // dd($groups);
+ //dd($groups);
 
-        return view('order')->with(['orders' => $orders, 'menus' => $menus, 'menuDesc' => $menuDesc, 'groups' => $groups]);
+
+    return ['menus' => $menus, 'menuDesc' => $menuDesc, 'groups' => $groups];
 
     }
 }
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -28,4 +28,38 @@
             @endfor
         </ul>
     @endforeach
+
+    <br>
+
+    <h1>Group orders</h1>
+    <br>
+    @foreach($groupOrders as $groupOrder)
+
+        <b>{{ $groupOrder->updated_at }}</b> - <i>{{ $groupOrder->send }}</i>; owner - <b>{{ $groupOwners[$groupOrder->user_id]['name'] }}</b>
+
+        <h3>WITH</h3>
+
+        @if(isset($groupGroups[$groupOrder->id]))
+            @for( $s = 0; $s < count($groupGroups[$groupOrder->id]) ; $s++)
+                <dd>{{ $groupGroups[$groupOrder->id][$s]['name'] }}</dd>
+            @endfor
+        @endif
+
+
+        <ul>
+            @for( $k = 0; $k < count($groupMenus[$groupOrder->id]) ; $k++)
+
+                <li>
+                    {{ $groupMenuDesc[$groupMenus[$groupOrder->id][$k]['menu_id']]['name'] .
+                     ' --- ' .
+                      $groupMenuDesc[$groupMenus[$groupOrder->id][$k]['menu_id']]['price']}}
+                </li>
+
+            @endfor
+        </ul>
+    @endforeach
+
+
+
+
 @endsection
